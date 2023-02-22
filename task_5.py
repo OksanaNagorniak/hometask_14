fe572b3474bad2bcbb459ebf1b10dca2af43bb7e
OksanaNagorniak <120406042+OksanaNagorniak@users.noreply.github.com>
2023-02-22 16:19:26 +0200

    Delete task_5.py

--- task_5.py
+++
@@ -1,8 +0,0 @@
-# Задание 5
-# Дан файл с произвольным текстом, нужно найти количество слов в файле и вывести пользователю
-
-f = open("Words_text.txt", 'r')
-data = f.read()
-words = data.split()
-print("Words in text file:", len(words))
-f.close()
